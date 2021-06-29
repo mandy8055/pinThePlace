3254e9ce99bf7df65d4ade9bc46f9a6c73067a4c
Mandy8055 <msankrit8055@gmail.com>
2021-06-30 00:14:55 +0530

    Renamed application and tried to have the workaround for mapbox

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "./App.css";
 import mapboxgl from "mapbox-gl";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 mapboxgl.workerClass =
+  // eslint-disable-next-line import/no-webpack-loader-syntax
   require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 function App() {
